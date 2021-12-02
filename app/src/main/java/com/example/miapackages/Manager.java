df6c52b808fbf480b5dc7641dc74c40d248f1ba8
Itamar Almog <itamaralmoghermel@gmail.con>
2021-12-02 12:03:57 +0200

    added addDriver function

--- app/src/main/java/com/example/miapackages/Manager.java
+++ app/src/main/java/com/example/miapackages/Manager.java
@@ -1,10 +1,9 @@
 package com.example.miapackages;
 
+import android.os.Bundle;
+
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-
-import android.os.Bundle;
-import android.util.Log;
 
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
@@ -21,26 +20,24 @@ public class Manager extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_manager);
         FirebaseFirestore db = FirebaseFirestore.getInstance();
+        addDriver(db , "itamard1","p@ssword");
     }
 
-
-//    protected void addNewProduct(FirebaseFirestore db){
-//        Map<String, Object> basketball = new HashMap<>();
-//        basketball.put("amount", 1);
-//        basketball.put("price", 25);
-//        basketball.put("supplier", "nike");
-//        db.collection("packages").add(basketball)
-//                .addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
-//                    @Override
-//                    public void onSuccess(DocumentReference documentReference) {
-//                    Log.d(TAG, "DocumentSnapshot added with ID: " + documentReference.getId());
-//                    }
-//                })
-//                .addOnFailureListener(new OnFailureListener() {
-//                    @Override
-//                    public void onFailure(@NonNull Exception e) {
-//                        Log.w(TAG, "Error adding document", e);
-//                    }
-//                });
-//    }
+    protected void addDriver(FirebaseFirestore db , String username,String password) {
+        Map<String, String> driver = new HashMap<>();
+        driver.put(username, password);
+        db.collection("users").document("drivers").collection("drivers").add(driver)
+                .addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
+                    @Override
+                    public void onSuccess(DocumentReference documentReference) {
+                        System.out.println("DocumentSnapshot added with ID: " + documentReference.getId());
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        System.out.println("Error adding document" + e);
+                    }
+                });
+    }
 }
