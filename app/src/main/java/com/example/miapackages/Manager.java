9f7fb4884c02714c7819b624b0d8b8ab169725f4
meytar14 <61780283+meytar14@users.noreply.github.com>
2021-12-02 18:59:06 +0200

    finish add driver and add item

--- app/src/main/java/com/example/miapackages/Manager.java
+++ app/src/main/java/com/example/miapackages/Manager.java
@@ -2,7 +2,7 @@ package com.example.miapackages;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-
+import android.view.View;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
@@ -15,7 +15,8 @@ import com.google.android.gms.tasks.Task;
 import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
-
+import com.google.firebase.firestore.SetOptions;
+import android.widget.EditText;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -26,8 +27,6 @@ public class Manager extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_manager);
 
-
-        FirebaseFirestore db = FirebaseFirestore.getInstance();
     }
 
     public void itemClick(View view){
@@ -91,27 +90,26 @@ public class Manager extends AppCompatActivity {
                 .addOnFailureListener(new OnFailureListener() {
                     @Override
                     public void onFailure(@NonNull Exception e) {
-                        System.out.println("Error adding document" + e);
+                        System.out.println("Error writing document"+e);
                     }
                 });
     }
 
 
-    protected void addDriver(FirebaseFirestore db , String username,String password) {
+    public void addDriver(View view)
+    {   //get the name and pass of the driver from editText
+        EditText driver_neme_view = (EditText) findViewById(R.id.driver_name);
+        String driverName = driver_neme_view.getText().toString();
+        EditText driver_pass_view = (EditText) findViewById(R.id.driver_password);
+        String driverPass = driver_pass_view.getText().toString();
+        //add the driver to the data base
+        FirebaseFirestore db = FirebaseFirestore.getInstance();
         Map<String, String> driver = new HashMap<>();
-        driver.put(username, password);
-        db.collection("users").document("drivers").collection("drivers").add(driver)
-                .addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
-                    @Override
-                    public void onSuccess(DocumentReference documentReference) {
-                        System.out.println("DocumentSnapshot added with ID: " + documentReference.getId());
-                    }
-                })
-                .addOnFailureListener(new OnFailureListener() {
-                    @Override
-                    public void onFailure(@NonNull Exception e) {
-                        System.out.println("Error adding document" + e);
-                    }
-                });
+        driver.put(driverName, driverPass);
+        db.collection("users").document("drivers").set(driver, SetOptions.merge());
+    }
+    protected void removeDriver(FirebaseFirestore db , String username)
+    {
+
     }
 }
