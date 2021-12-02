c5cd48804d8062efb20db868d17ee1ab3f761212
AndreyFriedman <andrey.fridman2001@gmail.com>
2021-12-02 10:59:31 +0200

    addNewProduct added

--- app/src/main/java/com/example/miapackages/Manager.java
+++ app/src/main/java/com/example/miapackages/Manager.java
@@ -1,8 +1,18 @@
 package com.example.miapackages;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class Manager extends AppCompatActivity {
 
@@ -10,5 +20,27 @@ public class Manager extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_manager);
+        FirebaseFirestore db = FirebaseFirestore.getInstance();
     }
+
+
+//    protected void addNewProduct(FirebaseFirestore db){
+//        Map<String, Object> basketball = new HashMap<>();
+//        basketball.put("amount", 1);
+//        basketball.put("price", 25);
+//        basketball.put("supplier", "nike");
+//        db.collection("packages").add(basketball)
+//                .addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
+//                    @Override
+//                    public void onSuccess(DocumentReference documentReference) {
+//                    Log.d(TAG, "DocumentSnapshot added with ID: " + documentReference.getId());
+//                    }
+//                })
+//                .addOnFailureListener(new OnFailureListener() {
+//                    @Override
+//                    public void onFailure(@NonNull Exception e) {
+//                        Log.w(TAG, "Error adding document", e);
+//                    }
+//                });
+//    }
 }
