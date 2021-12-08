6ea6c0f3987e874ee5504657a09856bb80bcec09
AndreyFriedman <andrey.fridman2001@gmail.com>
2021-12-08 18:29:13 +0200

    activity manager crush fixed

--- app/src/main/java/com/example/miapackages/Manager.java
+++ app/src/main/java/com/example/miapackages/Manager.java
@@ -4,6 +4,7 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.Activity;
+import android.text.TextUtils;
 import android.view.View;
 import android.os.Bundle;
 import android.util.Log;
@@ -43,6 +44,9 @@ public class Manager extends Activity implements AdapterView.OnItemSelectedListe
 
     @Override
     public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+        TextView err = (TextView) findViewById(R.id.err);
+        err.setVisibility(View.INVISIBLE);
+
         Spinner manager_options = (Spinner) findViewById(R.id.spinner);
         String opt = String.valueOf(manager_options.getSelectedItem());
 
@@ -126,23 +130,38 @@ public class Manager extends Activity implements AdapterView.OnItemSelectedListe
     }
 
     public void itemClick(View view){
+        int amountS = 0;
+        int priceS = 0;
+        String nameS = "abc";
+        String supplierS = "abc";
+
         EditText name = (EditText)findViewById(R.id.editTextName);
-        String nameS = name.getText().toString();
+        CharSequence str1 = name.getText().toString();
+        if (TextUtils.isEmpty(str1) != true)
+            nameS = name.getText().toString();
+
         EditText amount = (EditText)findViewById(R.id.editTextAmount);
-        int amountS = Integer.valueOf(amount.getText().toString());
+        CharSequence str2 = amount.getText().toString();
+        if (TextUtils.isEmpty(str2) != true)
+            amountS = Integer.valueOf(amount.getText().toString());
+
         EditText price = (EditText)findViewById(R.id.editTextPrice);
-        int priceS = Integer.valueOf(price.getText().toString());
+        CharSequence str3 = price.getText().toString();
+        if (TextUtils.isEmpty(str3) != true)
+            priceS = Integer.valueOf(price.getText().toString());
+
         EditText supplier = (EditText)findViewById(R.id.editTextSupplier);
-        String supplierS = supplier.getText().toString();
+        CharSequence str4 = supplier.getText().toString();
+        if (TextUtils.isEmpty(str4) != true)
+            supplierS = supplier.getText().toString();
 
-
-        System.out.println(nameS);
-        System.out.println(amountS);
-        System.out.println(priceS);
-        System.out.println(supplierS);
+        else {
+            TextView err = (TextView) findViewById(R.id.err);
+            err.setVisibility(View.VISIBLE);
+            return;
+        }
 
         FirebaseFirestore db = FirebaseFirestore.getInstance();
-
         prodManagment(db, nameS, amountS, priceS, supplierS);
     }
 
@@ -193,9 +212,17 @@ public class Manager extends Activity implements AdapterView.OnItemSelectedListe
                 });
     }
     public void deleteClick(View view){
+        String nameS = "";
         EditText name = (EditText)findViewById(R.id.editTextNameDel);
-        String nameS = name.getText().toString();
-        System.out.println(nameS);
+        //String nameS = name.getText().toString();
+        CharSequence str1 = name.getText().toString();
+        if (TextUtils.isEmpty(str1) != true)
+            nameS = name.getText().toString();
+        else {
+            TextView err = (TextView) findViewById(R.id.err);
+            err.setVisibility(View.VISIBLE);
+            return;
+        }
         FirebaseFirestore db = FirebaseFirestore.getInstance();
         deleteItem(db, nameS);
     }
@@ -218,10 +245,25 @@ public class Manager extends Activity implements AdapterView.OnItemSelectedListe
 
 
     public void addDriver(View view){   //get the name and pass of the driver from editText
-        EditText driver_neme_view = (EditText) findViewById(R.id.driver_name);
-        String driverName = driver_neme_view.getText().toString();
+        String driverName = "";
+        String driverPass = "";
+
+        EditText driver_name_view = (EditText) findViewById(R.id.driver_name);
+        CharSequence str1 = driver_name_view.getText().toString();
+        if (TextUtils.isEmpty(str1) != true)
+            driverName = driver_name_view.getText().toString();
+
         EditText driver_pass_view = (EditText) findViewById(R.id.driver_password);
-        String driverPass = driver_pass_view.getText().toString();
+        CharSequence str2 = driver_pass_view.getText().toString();
+        if (TextUtils.isEmpty(str2) != true)
+            driverPass = driver_pass_view.getText().toString();
+
+        else {
+            TextView err = (TextView) findViewById(R.id.err);
+            err.setVisibility(View.VISIBLE);
+            return;
+        }
+
         //add the driver to the data base
         FirebaseFirestore db = FirebaseFirestore.getInstance();
         Map<String, String> driver = new HashMap<>();
@@ -229,11 +271,19 @@ public class Manager extends Activity implements AdapterView.OnItemSelectedListe
         db.collection("users").document("drivers").set(driver, SetOptions.merge());
     }
     public void deleteDriver(View view) {
+        String driverName = "";
+
+        EditText driver_name_view = (EditText) findViewById(R.id.delete_driver_name);
+        CharSequence str1 = driver_name_view.getText().toString();
+        if (TextUtils.isEmpty(str1) != true)
+            driverName = driver_name_view.getText().toString();
+
+        else {
+            TextView err = (TextView) findViewById(R.id.err);
+            err.setVisibility(View.VISIBLE);
+            return;
+        }
         FirebaseFirestore db = FirebaseFirestore.getInstance();
-
-        EditText driver_neme_view = (EditText) findViewById(R.id.delete_driver_name);
-        String driverName = driver_neme_view.getText().toString();
-
         Map<String, Object> updates = new HashMap<>();
         updates.put(driverName, FieldValue.delete());
 
