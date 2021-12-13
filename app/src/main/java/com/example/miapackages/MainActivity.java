6e7373d1df4547fd2b9bbdb5e7623e05cd96a780
Itamar Almog <itamaralmoghermel@gmail.con>
2021-12-13 12:38:26 +0200

    chnges in mainactivity

--- app/src/main/java/com/example/miapackages/MainActivity.java
+++ app/src/main/java/com/example/miapackages/MainActivity.java
@@ -3,25 +3,45 @@ package com.example.miapackages;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+
 public class MainActivity extends AppCompatActivity {
+
+    EditText userName;
+    EditText password;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        init();
+    }
+    public void init(){
+        userName =(EditText)findViewById(R.id.userName);
+        password =(EditText)findViewById(R.id.password);
     }
 
     public void onManager(View view){
         Intent intent = new Intent(this, Manager.class);
         startActivity(intent);
     }
+
     public void onClient(View view){
-        Intent intent = new Intent(this, Client.class);
-        startActivity(intent);
+        String document = "clients";
+        boolean isClient = searchInDataSet(document);
+        if(isClient) {
+            Intent intent = new Intent(this, Client.class);
+            startActivity(intent);
+        }
+        else{
+
+        }
     }
     public void onDriver(View view){
         Intent intent = new Intent(this, Driver.class);
@@ -30,6 +50,19 @@ public class MainActivity extends AppCompatActivity {
     public void register(View view){
         Intent intent = new Intent(this, Register.class);
         startActivity(intent);
+    }
+
+    public boolean searchInDataSet(String document){
+        FirebaseFirestore db = FirebaseFirestore.getInstance();
+//        db.collection("Users").document(document).;
+//        db.collection("Users");
+        DocumentReference users = db.collection("Users").document(document);
+        //users.
+
+
+
+
+        return true;
     }
 
 
